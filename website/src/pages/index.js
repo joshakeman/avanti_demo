2c2cb957e16c6f8a16d02a18a3406efa3459c9f6
Josh Akeman <joshakeman@gmail.com>
2020-04-25 14:44:16 -0400

    changed logo to normal image

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -9,6 +9,7 @@ import '../styles/home.css'
 import Img from "gatsby-image"
 import StyledLogo from '../components/logo'
 import mountains from '../images/wyoming_mountain_2_extra_cropped.jpg'
+import logo from '../images/avanti-logo.jpg'
 
 const IndexPage = () => (
 
@@ -23,7 +24,10 @@ const IndexPage = () => (
         <p className="buffer">
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
         </p>
-        <StyledLogo />
+        {/* <StyledLogo /> */}
+        <div style={{width: '100%', display:'flex', justifyContent:'center'}}>
+          <img src={logo} style={{width:300, margin:'0 auto', opacity: '0.8'}} />
+        </div>
       </div>
       <div className="relative-wrapper">
         <BackgroundSection />
