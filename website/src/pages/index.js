092aebad62ced915a5b16cfcda458efc0d2f07c3
Josh Akeman <joshakeman@gmail.com>
2020-04-25 14:55:47 -0400

    quick tweak

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -22,7 +22,7 @@ const IndexPage = () => (
         frontier of banking
         </h2>
         <p className="buffer">
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
         </p>
         {/* <StyledLogo /> */}
         <div style={{width: '100%', display:'flex', justifyContent:'center'}}>
